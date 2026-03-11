43bac8e51d1178f8efe62c3731863cb89c2ea3c7
Denis Efremov <efremov@linux.com>
2026-03-12 01:15:24 +0400

    feat(install): Add install-time security warning
    Warn users during module installation that this module patches adbd
    to run as root and skips USB authentication, weakening device security.

---
+++ customize.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+
+ui_print "************************************"
+ui_print " WARNING: This module patches adbd"
+ui_print " to always run as root and skips"
+ui_print " USB authentication."
+ui_print " "
+ui_print " This weakens device security."
+ui_print " Use only for debugging and"
+ui_print " disable when not needed."
+ui_print "************************************"
